e85ee399aafaabab659218579a95c3288cd875cb
courtcoder <courtney1187@icloud.com>
2022-05-02 14:30:54 -0400

    added features to task form

--- assets/css/js/script 2.js
+++ assets/css/js/script 2.js
@@ -1,13 +1,11 @@
-var formE1 = document.querySelector("#task-form");
+var buttonE1 = document.querySelector("#save-task");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
-    event.preventDefault();
-    
+var createTaskHandler = function() {
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItemE1);
 };
 
-formE1.addEventListener("submit", createTaskHandler);
+buttonE1.addEventListener("click", createTaskHandler);
